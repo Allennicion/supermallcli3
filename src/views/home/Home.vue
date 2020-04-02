15a8e53a86eb126700f52b6ae78173c902c9fa5c
Allennicion <Allennicion@users.noreply.github.com>
2020-04-02 21:18:22 +0800

    2020

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,35 +3,141 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
-    <swiper>
-      <swiper-item v-for="item in banners">
-        <a :href="item.link">
-          <img :src="item.image" alt="">
-        </a>
-      </swiper-item>
-    </swiper>
+    <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control :title="['流行','新款','精品']" class="tab-control"></tab-control>
+    <ul>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+      <li>&</li>
+    </ul>
   </div>
 </template>
 
 <script>
   import NavBar from 'components/common/navbar/NavBar.vue'
-  import {getHomeMultidata} from 'network/home'
-  import {Swiper,SwiperItem} from 'components/common/swiper'
+  import {getHomeMultidata,getHomeGoods} from 'network/home'
+  import HomeSwiper from 'views/home/childComps/HomeSwiper.vue'
   import RecommendView from 'views/home/childComps/RecommendView.vue'
+  import FeatureView from 'views/home/childComps/FeatureView.vue'
+  import TabControl from 'components/content/tabControl/TabControl.vue'
 
   export default {
     name: "Home",
     components: {
       NavBar,
-      'swiper': Swiper,
-      'swiper-item': SwiperItem,
-      RecommendView
+      HomeSwiper,
+      RecommendView,
+      FeatureView,
+      TabControl
     },
     data() {
       return {
         banners: [],
-        recommends: []
+        recommends: [],
+        goods:  {
+          'pop': {page: 0, list: []},
+          'news': {page: 0, list: []},
+          'sell': {page: 0, list: []}
+        }
       }
     },
     created() {
@@ -45,8 +151,24 @@
 </script>
 
 <style scoped>
+  #home{
+    padding-top: 44px;
+  }
+
   .home-nav{
     background-color: var(--color-tint);
     color: #fff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+
+    z-index: 9;
+  }
+
+  .tab-control{
+    position: sticky;
+    top: 44px;
   }
 </style>
